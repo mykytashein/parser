180e896d53e763a15e4b5f714e3d94155e92d5de
mykytashein <mrshein@icloud.com>
2024-05-01 15:18:43 +0300

    initial commit

---
+++ src/test/java/com/shein/parser/ParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shein.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
