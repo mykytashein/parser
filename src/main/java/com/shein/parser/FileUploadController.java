49771ceeaa8348c283b634686d40e7b66ccc7e90
mykytashein <mrshein@icloud.com>
2024-05-26 19:49:58 +0300

    added xml processing added json/xml content handling to the server side added content table to the client side

--- src/main/java/com/shein/parser/FileUploadController.java
+++ src/main/java/com/shein/parser/FileUploadController.java
@@ -6,9 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//import javax.xml.parsers.DocumentBuilderFactory;
+import org.json.XML;
+import org.json.JSONObject;
+import com.github.wnameless.json.flattener.JsonFlattener;
 
 import java.io.IOException;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 @Controller
 public class FileUploadController {
@@ -20,11 +25,31 @@ public class FileUploadController {
         if (file != null && !file.isEmpty()) {
             try {
                 // Parse the file
+                String contentType = file.getContentType();
+                String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+                JSONObject json;
+                assert contentType != null;
+                if (contentType.contains("/xml")) {
+                    json = XML.toJSONObject(content); // Convert XML to JSON
+                } else {
+                    json = new JSONObject(content); // Parse JSON
+                }
+
+                // Flatten the JSON
+                String flatJson = JsonFlattener.flatten(json.toString());
                 ObjectMapper objectMapper = new ObjectMapper();
-                Map<String, Object> jsonMap = objectMapper.readValue(file.getInputStream(), Map.class);
+                Map<String, Object> flatJsonMap = objectMapper.readValue(flatJson, Map.class);
+
+                // Split the keys and create a new map for each part
+                List<List<String>> tableData = new ArrayList<>();
+                for (Map.Entry<String, Object> entry : flatJsonMap.entrySet()) {
+                    List<String> row = new ArrayList<>(Arrays.asList(entry.getKey().split("\\.")));
+                    row.add(entry.getValue().toString());
+                    tableData.add(row);
+                }
 
                 // Add the parsed data to the model
-                model.addAttribute("jsonContent", jsonMap);
+                model.addAttribute("tableData", tableData);
 
                 // Redirect with success message
                 redirectAttributes.addFlashAttribute("message",
